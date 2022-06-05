0d8fbe9e54ff6550e9c69ce40b982929f338a196
Jose Miola <jose.miola@aegro.com.br>
2022-06-05 20:07:40 -0300

    Criado Farms

---
+++ src/app/farms/farms.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FarmsRoutingModule } from './farms-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FarmsRoutingModule
+  ]
+})
+export class FarmsModule { }
